04760597d6ff51da654c2aca9e06364ad9bf5e96
Alejandro Sanchez <aalejo@gmail.com>
2021-08-04 18:11:56 -0400

    made the video player more responsive for the intro

--- src/js/components/intro/Intro.js
+++ src/js/components/intro/Intro.js
@@ -5,7 +5,7 @@ import "./intro.css";
 
 const Intro = ({ onClose, url, playing }) => {
     return <div className="exercise-intro">
-        <ReactPlayer width="100%" url={url} playing={playing} />
+        <ReactPlayer width="100%" height="50vh" url={url} playing={playing} controls={true} />
         <button type="button" className="btn btn-success w-100" onClick={() => onClose()}>
             Close video and start exercise
             <i className="fas fa-arrow-right ml-2"></i>
